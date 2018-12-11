9f7fcedfa6370c76a5276a95f28b6c9e8555b77d
David Martin <david.martin@fundingcircle.com>
2018-12-11 18:34:37 +0000

    updated project description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject nrepl-rebl "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "nrepl middleware for REBL"
+  :url "https://github.com/DaveWM/nrepl-rebl"
   :license {:name "GPL V3"
             :url "https://www.gnu.org/licenses/gpl-3.0.en.html"}
   :dependencies [[org.clojure/clojure "1.10.0-RC4" :scope "provided"]
